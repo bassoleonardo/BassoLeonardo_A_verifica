4e2f360fe05818d5e1061e8059260b633c8ef063
leonardo <leonardo.basso@barsanti.edu.it>
2019-02-16 10:40:41 +0100

    16/02/2019 - funzionamento led accertato

--- BassoLeonardo_A-verifica/BassoLeonardo_A-verifica.ino
+++ BassoLeonardo_A-verifica/BassoLeonardo_A-verifica.ino
@@ -1,11 +1,19 @@
 int led1 = 13;
-int
+int led2 = 11;
+int led3 = 7;
+int led4 = 5;
 void setup() {
   // put your setup code here, to run once:
-
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT);
+pinMode(led4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+digitalWrite(led1, HIGH);
+digitalWrite(led2, HIGH);
+digitalWrite(led3, HIGH);
+digitalWrite(led4, HIGH);
 }
